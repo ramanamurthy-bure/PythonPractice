4bfa06fd47bc25309180b78414205d2951cd6a9e
ramanamurthy-bure <ramanamurthy.bure@gmail.com>
2022-07-20 18:51:48 +0530

    New programs

---
+++ HackerRank/31_diagnolDiffs.py
@@ -0,0 +1,88 @@
+'''
+Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+For example, the square matrix  is shown below:
+
+1 2 3
+4 5 6
+9 8 9
+The left-to-right diagonal = . The right to left diagonal = . Their absolute difference is .
+
+Function description
+
+Complete the  function in the editor below.
+
+diagonalDifference takes the following parameter:
+
+int arr[n][m]: an array of integers
+Return
+
+int: the absolute diagonal difference
+Input Format
+
+The first line contains a single integer, , the number of rows and columns in the square matrix .
+Each of the next  lines describes a row, , and consists of  space-separated integers .
+
+Constraints
+
+Output Format
+
+Return the absolute difference between the sums of the matrix's two diagonals as a single integer.
+
+Sample Input
+
+3
+11 2 4
+4 5 6
+10 8 -12
+Sample Output
+
+15
+Explanation
+
+The primary diagonal is:
+
+11
+   5
+     -12
+Sum across the primary diagonal: 11 + 5 - 12 = 4
+
+The secondary diagonal is:
+
+     4
+   5
+10
+Sum across the secondary diagonal: 4 + 5 + 10 = 19
+Difference: |4 - 19| = 15
+
+Note: |x| is the absolute value of x
+
+'''
+import os
+
+
+def diagonalDifference(arr):
+    # Write your code here
+    diag1 = 0
+    diag2 = 0
+    n = len(arr)
+    #print(n)
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                print(arr[i][j])
+                diag1 += arr[i][j]
+            if j == n-i-1:
+                print(arr[i][j])
+                diag2 += arr[i][j]
+    #print(diag1)
+    #print(diag2)
+    return abs(diag1 - diag2)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    arr = []
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+    result = diagonalDifference(arr)
+    print(str(result) + '\n')
