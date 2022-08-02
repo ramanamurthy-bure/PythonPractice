c4e9622eabcad325519887a68fd7094337a5ea11
ramanamurthy-bure <ramanamurthy.bure@gmail.com>
2022-08-02 19:53:33 +0530

    New programs

--- DS/linkedlist_ds.py
+++ DS/linkedlist_ds.py
@@ -13,12 +13,11 @@ class LinkedList:
 
     def insert_at_end(self, data):
         if self.head is None:
-            print("Linked list is empty!")
             self.head = Node(data, None)
             return
 
         itr = self.head
-        while itr:
+        while itr.next:
             itr = itr.next
 
         itr.next= Node(data, None)
@@ -67,6 +66,9 @@ class LinkedList:
         while itr:
             if counter == index - 1:
                 itr.next = itr.next.next
+                break
+
+            itr =  itr.next
             counter += 1
 
     def insert_at(self, index,data):
@@ -84,12 +86,29 @@ class LinkedList:
                 node = Node(data,itr.next)
                 itr.next  = node
                 break
+
             itr =  itr.next
             counter += 1
 
 if __name__ == '__main__':
     lkdl = LinkedList()
-    lkdl.insert_at_beginning(23)
-    lkdl.insert_at_beginning(33)
-    lkdl.insert_at_beginning(43)
+    lkdl.insert_at_beginning(1)
+    lkdl.insert_at_beginning(2)
+    lkdl.insert_at_beginning(3)
+    lkdl.insert_at_beginning(4)
+    lkdl.insert_at_beginning(5)
+    lkdl.display()
+    lkdl.remove_at(1)
+    print("After removing the element at index 1")
+    lkdl.display()
+    lkdl.insert_at(2,3)
+    print("After inserting 3 at index 2")
+    lkdl.display()
+    lkdl.insert_values([10,11,12,13,14,15])
+    print("After inserting list of data to linkedlist")
+    lkdl.display()
+    lkdl.insert_at_end(16)
+    lkdl.insert_at_end(17)
+    lkdl.insert_at_end(18)
+    print("After inserting the elements at the end")
     lkdl.display()
